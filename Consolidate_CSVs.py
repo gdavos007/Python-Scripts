d00e591a8abb8840b1b729dd2f96cec5b7979761
Ganesh Krishnan <gkrishnan7@gmail.com>
2024-08-02 09:47:12 -0700

    Update Consolidate_CSVs.py
    Took out the SCP connection (GK0824)

--- Consolidate_CSVs.py
+++ Consolidate_CSVs.py
@@ -1,34 +1,14 @@
 import pandas as pd
 import holidays
-import paramiko
 from datetime import datetime, timedelta
 import os
 
-hostname = "12345"
-username = os.getenv("WINSCP_USERNAME")
-password = os.getenv("WINSCP_PASSWORD")
-
-if not username or not password:
-    print("Username or password not set in environment variables.")
-    exit()
-
-# Login into SCP
-try:
-    ssh_client = paramiko.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_client.connect(hostname=hostname, username=username, password=password)
-
-    sftp_client = ssh_client.open_sftp()
-except paramiko.SSHException as e:
-    print(f"SSH connection error: {e}") 
-    exit()
-
 # Define the range of dates for the files that need processing
-start_date = datetime(2021, 1, 18)
-end_date = datetime(2024, 7, 20)
+start_date = datetime(2024, 3, 18)
+end_date = datetime(2024, 7, 16)
 
 # Define the directory where the files are stored
-remote_dir = '/directory_path/folder_of_interest/'
+local_dir = '/path/to/local-directory/'
 
 # Define US public holidays for 2024 and beyond
 us_holidays = holidays.UnitedStates(years=[2024, 2025, 2026, 2027, 2028])  # Add more years as needed
@@ -40,20 +20,22 @@ dataframes = []
 current_date = start_date
 while current_date <= end_date:
     date_str = current_date.strftime('%Y-%m-%d')
-    file_name = f"final_{date_str}.csv"
-    remote_file_path = os.path.join(remote_dir, file_name)
+    file_name = f"file_{date_str}.csv"
+    local_file_path = os.path.join(local_dir, file_name)
     
     try:
-        with sftp_client.file(remote_file_path, 'r') as remote_file:
+        if os.path.exists(local_file_path):
             # Read the CSV file into a DataFrame
-            df = pd.read_csv(remote_file)
+            df = pd.read_csv(local_file_path)
             # Update the 'Date' column to match the date from the file name
             df['Date'] = date_str
             # Add the 'HOLIDAY' column
             df['HOLIDAY'] = 1 if current_date in us_holidays else 0
             dataframes.append(df)
+        else:
+            print(f"File {local_file_path} does not exist.")
     except IOError:
-        print(f"File {remote_file_path} does not exist.")
+        print(f"Error reading file {local_file_path}.")
     
     # Move to the next day
     current_date += timedelta(days=1)
@@ -62,11 +44,7 @@ while current_date <= end_date:
 if dataframes:
     combined_df = pd.concat(dataframes)
     # Save the combined DataFrame to a new CSV file
-    combined_df.to_csv('/path/to/combined_files.csv', index=False)
-    print("All files have been consolidated into 'combined_files.csv'")
+    combined_df.to_csv('/path/to/combined_file.csv', index=False)
+    print("All files have been consolidated into 'combined_file.csv'")
 else:
     print("No files were found or processed.")
-
-# Close the SFTP client
-sftp_client.close()
-ssh_client.close()
